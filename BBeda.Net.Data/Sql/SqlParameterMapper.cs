39e1cf3f0ecc5618959422b09ad2d6c2d5ffc2bb
bbeda <bogdan.beda@hotmail.com>
2016-05-10 00:09:36 +0100

    - setup - created projects - added code to work with stored procedures in entity framework

---
+++ BBeda.Net.Data/Sql/SqlParameterMapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBeda.Net.Data.Sql
+{
+    public class SqlParameterMapper
+    {
+        public static IEnumerable<SqlParameter> ToSqlParameters(object parameter)
+        {
+            if (parameter == null)
+            {
+                yield break;
+            }
+
+            var properties = parameter.GetType().GetProperties(BindingFlags.Public | BindingFlags.GetProperty | BindingFlags.Instance).ToList();
+            foreach (var prop in properties)
+            {
+                var name = prop.Name;
+                name = "@" + string.Join(string.Empty, name.Take(1).Select(c => char.ToLower(c))) + string.Join(string.Empty, name.Skip(1));
+                var value = prop.GetValue(parameter);
+                yield return new SqlParameter(name, value ?? DBNull.Value);
+            }
+        }
+
+        public static string GetParametersString(IEnumerable<SqlParameter> parameters)
+        {
+            return string.Join(",", parameters.Select(p => p.ParameterName));
+        }
+    }
+}
---
+++ BBeda.Net.EntityFramework.Sql/Sql/DataContextHelper.cs
@@ -0,0 +1,41 @@
+﻿using BBeda.Net.Data.Sql;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBeda.Net.Data.EntityFramework.Sql
+{
+    public static class DataContextHelper
+    {
+        public static DbRawSqlQuery<TElement> ExecuteStoredProcedure<TElement>(DbContext context, string storeProcName, object parameters = null)
+        {
+            if (parameters == null)
+            {
+                return context.Database.SqlQuery<TElement>(string.Format("EXEC {0}", storeProcName));
+            }
+
+            var sqlParams = SqlParameterMapper.ToSqlParameters(parameters).ToArray();
+            var paramsString = SqlParameterMapper.GetParametersString(sqlParams);
+            return context.Database.SqlQuery<TElement>(string.Format("EXEC {0} {1}", storeProcName, paramsString), sqlParams);
+        }
+
+        public static async Task<TElement> ExecuteSingleElementStoredProcedure<TElement>(DbContext context, string storeProcName, object parameters = null)
+        {
+            return (await ExecuteIEnumerableStoredProcedure<TElement>(context, storeProcName, parameters)).FirstOrDefault();
+        }
+
+        public static async Task<IEnumerable<TElement>> ExecuteIEnumerableStoredProcedure<TElement>(DbContext context, string storeProcName, object parameters = null)
+        {
+            return await ExecuteStoredProcedure<TElement>(context, storeProcName, parameters).ToListAsync();
+        }
+
+        public static async Task ExecuteStoredProcedure(DbContext context, string storeProcName, object parameters = null)
+        {
+            (await ExecuteIEnumerableStoredProcedure<object>(context, storeProcName, parameters)).FirstOrDefault();
+        }
+    }
+}
