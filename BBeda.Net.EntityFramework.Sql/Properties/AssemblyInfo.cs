d1442414c228bd0e4b5594e518c6d086e666d79d
bbeda <bogdan.beda@hotmail.com>
2016-05-10 00:23:45 +0100

    - updated assembly info

--- BBeda.Net.EntityFramework.Sql/Properties/AssemblyInfo.cs
+++ BBeda.Net.EntityFramework.Sql/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BBeda.Net.Data.EntityFramework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Entity Framework Extras")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BBeda.Net.Data.EntityFramework")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("Bogdan Beda")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
